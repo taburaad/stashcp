4f9d403744ca6252382e25d75736435299a17772
Tony Aburaad <tony.aburaad@gmail.com>
2016-08-03 13:11:45 -0500

    test

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,1 @@
-eck=$(md5sum 2gb_filetar | awk '{print $1;}')
-string="7d05ef0790d891f918eeafec40185fd8"
-      if [ "$check" != "$string" ]
-      then
-              echo "FILE_NOT_EQUAL"
-      else
-      echo "GOOD"
-      fi
-
-      ls -l 2gb_file.tar
-      rm -f 2gb_file.tar.
+print "good"

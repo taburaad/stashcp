9f177dab10cea4926c4db7fe4c698d5a0e008a08
Tony Aburaad <tony.aburaad@gmail.com>
2016-09-20 16:14:28 -0500

    working version

--- timeout.py
+++ timeout.py
@@ -1,19 +1,27 @@
-import argparse
+import optparse
 import os
 import time
 import subprocess
 import multiprocessing
 
 
-parser = argparse.ArgumentParser(description='Run and monitor xrdcp command')
-parser.add_argument('-t', dest='timeout', type=int)
-parser.add_argument('-f', dest='filename')
-parser.add_argument('-d', dest='diff', type=int)
-parser.add_argument('-s', dest='expSize', type=int)
-parser.add_argument('-x', dest='xrdebug')
-parser.add_argument('-c', dest='cache')
-parser.add_argument('-z', dest='destination')
-results = parser.parse_args()
+parser = optparse.OptionParser()
+parser.add_option('-t', dest='timeout', type=int)
+parser.add_option('-f', dest='filename')
+parser.add_option('-d', dest='diff', type=int)
+parser.add_option('-s', dest='expSize', type=int)
+parser.add_option('-x', dest='xrdebug')
+parser.add_option('-c', dest='cache')
+parser.add_option('-z', dest='destination')
+results,opts = parser.parse_args()
+
+print results.timeout
+print results.filename
+print results.diff
+print results.expSize
+print results.xrdebug
+print results.cache
+print results.destination
 
 def start_watchdog(xrdcp,timeout=results.timeout,filename=results.filename,diff=results.diff,expSize=results.expSize):
     prevSize=0
@@ -36,9 +44,7 @@ def start_watchdog(xrdcp,timeout=results.timeout,filename=results.filename,diff=
         else:
             xrdcp.kill()
             newSize=expSize
-
 filepath=results.cache+":1094//"+ results.filename
-
 if results.xrdebug=="1":
     command="xrdcp -d 2 --nopbar -f " + filepath + " " + results.destination
 else:
@@ -47,18 +53,13 @@ else:
 filename="./"+results.filename.split("/")[-1]
 if os.path.isfile(filename):
     os.remove(filename)
-#run xrdcp command 
 xrdcp=subprocess.Popen([command ],shell=True,stdout=subprocess.PIPE)
 time.sleep(1)
-#start watchdog
 watchdog=multiprocessing.Process(target=start_watchdog,args=[xrdcp])
 watchdog.start()
 
 streamdata=xrdcp.communicate()[0]
 
-#return xrdcp exit code
 xrd_exit=xrdcp.returncode
 print "xrdcp exit code: ", xrd_exit
-
-#stop watchdog
 watchdog.terminate()
